46ed5217c2046883629a3d20f2f35e8e455675a2
RomaSamkov <Romansamkov.rs@gmail.com>
2023-11-05 15:43:44 +0200

    add if and and

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 name = 'Python'
 student = "coder"
 user_name = input('Write your name:\n')
+user_age = int(input('Write your age'))
 
 
-if user_name == 'Mark':
+if user_name == 'Mark' and user_age > 10:
     print('Hello Mark')
+    print(f"Your age is {user_age} years")
 elif user_name == 'Sponge':
     print('Hello Sponge')
 else:
