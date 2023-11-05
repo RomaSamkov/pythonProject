fa5b3795799c21edebbfe499e52afd26bec0a206
RomaSamkov <Romansamkov.rs@gmail.com>
2023-11-05 15:55:02 +0200

    add task

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 name = 'Python'
 student = "coder"
-user_name = input('Write your name:\n')
-user_age = int(input('Write your age'))
+# user_name = input('Write your name:\n')
+# user_age = int(input('Write your age'))
 
 
-if user_name == 'Mark' and user_age > 10:
-    print('Hello Mark')
-    print(f"Your age is {user_age} years")
-elif user_name == 'Sponge':
-    print('Hello Sponge')
-else:
-    print(f"Hello {user_name}!")
+# if user_name == 'Mark' and user_age > 10:
+#     print('Hello Mark')
+#     print(f"Your age is {user_age} years")
+# elif user_name == 'Sponge':
+#     print('Hello Sponge')
+# else:
+#     print(f"Hello {user_name}!")
 
 
+cost = 110
 
+if cost > 100:
+    cost = cost - cost * 0.03
+elif cost > 500:
+    cost = cost - cost * 0.05
+if cost > 1000:
+    cost = cost - cost * 0.1
+print(cost)
