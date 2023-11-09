56fc4cc69d10595693f1e23f4eaa68ac23c1f6c3
RomaSamkov <Romansamkov.rs@gmail.com>
2023-11-09 16:34:51 +0200

    add def task1

--- function.py
+++ function.py
@@ -4,7 +4,12 @@
 #
 # sum_sum(1,2,3,4,5,6)
 
-def func():
-    return
+def func_max(num_list):
+    max = 0
+    for i in num_list:
+        if i > max:
+            max = i
+    return max
 
-func()
+r = func_max([1,2,3,4,5,6,7,11])
+print(r)
