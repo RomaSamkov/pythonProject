bfbeaac4e33d1cdc5c23382379cbffbe68a2d5ad
RomaSamkov <Romansamkov.rs@gmail.com>
2023-11-09 16:38:35 +0200

    add def task2

--- function.py
+++ function.py
@@ -4,12 +4,18 @@
 #
 # sum_sum(1,2,3,4,5,6)
 
-def func_max(num_list):
-    max = 0
-    for i in num_list:
-        if i > max:
-            max = i
-    return max
+# def func_max(num_list):
+#     max = 0
+#     for i in num_list:
+#         if i > max:
+#             max = i
+#     return max
+#
+# r = func_max([1,2,3,4,5,6,7,11])
+# print(r)
 
-r = func_max([1,2,3,4,5,6,7,11])
-print(r)
+def func_char(string):
+    return len(string)
+
+result = func_char('JavaScript and Python')
+print(result)
