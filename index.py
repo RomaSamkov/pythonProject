853f0e00d64b5f5fc12e8587f306d3714d352b90
RomaSamkov <Romansamkov.rs@gmail.com>
2023-11-14 17:06:35 +0200

    add module

--- index.py
+++ index.py
@@ -1,5 +1,13 @@
-try:
-    with open('data/file.txt', 'r') as file:
-        print(file.read())
-except FileNotFoundError:
-    print('File not found')
+# try:
+#     with open('data/file.txt', 'r') as file:
+#         print(file.read())
+# except FileNotFoundError:
+#     print('File not found')
+
+import datetime
+import sys, os, platform
+
+print(datetime.datetime.now().time())
+# print(sys.path)
+print(os.name)
+print(platform.system())
