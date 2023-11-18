4faf50caa6c8ebf730e6b4f71d71cfcdcef996d2
RomaSamkov <Romansamkov.rs@gmail.com>
2023-11-18 16:23:53 +0200

    add class to myModule

--- myModule.py
+++ myModule.py
@@ -7,3 +7,12 @@ def hello():
 
 def sum_num(a, b, c):
     print(a+b+c)
+
+
+class Car:
+    name = 'Drive'
+    def get_drive(self):
+        print(f'Hello {name} your car name is {self.name} !')
+
+car = Car()
+car.get_drive()
