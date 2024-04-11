69207d0309b4278cb73b5a62036801539321c4c5
RomaSamkov <Romansamkov.rs@gmail.com>
2024-04-11 12:05:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 fruits = ['banana', 'orange', 'apple', 'coconut']
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-for number in numbers:
-    print(number)
-
-
-
+print(list(range(1, 10, 2)))
 print(fruits[0:3])
+print(f'{numbers=}')
+print(f'{fruits=}')
+
